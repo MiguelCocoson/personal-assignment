76ac4b568f7fe045d13403e7031f1fa0a214ccb4
Miguel Marin <miguelmarinjavier5@gmail.com>
2023-02-11 19:45:11 -0700

    new3

--- app.js
+++ app.js
@@ -1,27 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
-
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
   .use(bodyParser.json())
-  app.use((req, res, next) => {
+  .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    res.setHeader(
-      'Access-Control-Allow-Methods',
-      'GET, POST, PATCH, PUT, DELETE, OPTIONS'
-    );
     next();
   })
   .use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
+
+mongodb.initDb((err) => {
   if (err) {
     console.log(err);
   } else {
