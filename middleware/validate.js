c37b6c338f107417bf569e099fe0ca6ffab793c0
Miguel Marin <miguelmarinjavier5@gmail.com>
2023-02-11 19:36:29 -0700

    new6

---
+++ middleware/validate.js
@@ -0,0 +1,24 @@
+const validator = require('../helpers/validate.js');
+
+const savePost = (req, res, next) => {
+  const validationRule = {
+    firstName: 'required|string',
+    lastName: 'required|string',
+    post: 'required|string'
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
+
+module.exports = {
+  savePost
+};
