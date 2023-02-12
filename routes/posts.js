3ad85e1d3c80613684a909dca7790290833457b8
Miguel Marin <miguelmarinjavier5@gmail.com>
2023-02-11 20:16:05 -0700

    new6

--- routes/posts.js
+++ routes/posts.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router = express.Router();
 
-const contactsController = require('../controllers/posts.js');
+const postsController = require('../controllers/posts.js');
 
 router.get('/', postsController.getAll);
 
