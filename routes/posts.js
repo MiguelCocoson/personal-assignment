293925940589f4d0b0b56782173d0691761647d0
Miguel Marin <miguelmarinjavier5@gmail.com>
2023-02-11 20:21:08 -0700

    NEW6

--- routes/posts.js
+++ routes/posts.js
@@ -2,16 +2,17 @@
 const express = require('express');
 const router = express.Router();
 
-const postsController = require('../controllers/posts.js');
+const postsController = require('../controllers/posts');
+const validation = require('../middleware/validate');
 
 router.get('/', postsController.getAll);
 
 router.get('/:id', postsController.getSingle);
 
-router.post('/', postsController.createPost);
+router.post('/', validation.savePost, postsController.createpost);
 
-router.put('/:id', postsController.updatePost);
+router.put('/:id', validation.savePost, postsController.updatepost);
 
-router.delete('/:id', postsController.deletePost);
+router.delete('/:id', postsController.deletepost);
 
 module.exports = router;
