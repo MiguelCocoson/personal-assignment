d4ef6016a23dab345a1e724f4d6a704d161fb580
Miguel Marin <miguelmarinjavier5@gmail.com>
2023-02-11 20:27:46 -0700

    week6

--- routes/posts.js
+++ routes/posts.js
@@ -9,10 +9,10 @@ router.get('/', postsController.getAll);
 
 router.get('/:id', postsController.getSingle);
 
-router.post('/', validation.savePost, postsController.createpost);
+router.post('/', validation.savePost, postsController.createPost);
 
-router.put('/:id', validation.savePost, postsController.updatepost);
+router.put('/:id', validation.savePost, postsController.updatePost);
 
-router.delete('/:id', postsController.deletepost);
+router.delete('/:id', postsController.deletePost);
 
 module.exports = router;
